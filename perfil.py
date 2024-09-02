8bbd6751d21d5060dc2d00959b85ee2eca116304
Desenvolvedorpythonanalista <lachimolalala61@gmail.com>
2024-09-01 22:52:35 -0300

    Update perfil.py

--- perfil.py
+++ perfil.py
@@ -26,34 +26,16 @@ def create_table():
     conn = create_connection()
     if conn is not None:
         try:
-            cursor = conn.cursor()
-            cursor.execute('''
+            conn.execute('''
                 CREATE TABLE IF NOT EXISTS profiles (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    company_name TEXT,
-                    website TEXT,
-                    client_type TEXT,
-                    contact_name TEXT,
-                    email TEXT,
-                    phone TEXT,
-                    address TEXT,
-                    no_physical_address BOOLEAN,
-                    capital TEXT,
-                    desired_revenue TEXT,
-                    services TEXT,
-                    payment_methods TEXT,
-                    source TEXT,
-                    business_field TEXT,
-                    business_type TEXT,
-                    context TEXT,
-                    return_time TEXT,
-                    market_analysis BOOLEAN,
-                    difficulties TEXT,
-                    cnpj_or_cpf TEXT,
-                    logo_path TEXT,
-                    pdf_path TEXT,
-                    video_path TEXT,
-                    employees TEXT
+                    company_name TEXT, website TEXT, client_type TEXT,
+                    contact_name TEXT, email TEXT, phone TEXT, address TEXT,
+                    no_physical_address BOOLEAN, capital TEXT, desired_revenue TEXT,
+                    services TEXT, payment_methods TEXT, source TEXT, business_field TEXT,
+                    business_type TEXT, context TEXT, return_time TEXT, market_analysis BOOLEAN,
+                    difficulties TEXT, cnpj_or_cpf TEXT, logo_path TEXT, pdf_path TEXT,
+                    video_path TEXT, employees TEXT
                 )
             ''')
             conn.commit()
@@ -81,10 +63,6 @@ def insert_data(data, logo_path=None, pdf_path=None, video_path=None):
             query = f'INSERT INTO profiles ({", ".join(columns)}) VALUES ({placeholders})'
 
             values = [json.dumps(item) if isinstance(item, list) else item for item in data]
-            values.append(logo_path if logo_path else None)
-            values.append(pdf_path if pdf_path else None)
-            values.append(video_path if video_path else None)
-
             cursor.execute(query, values)
             conn.commit()
         except sqlite3.Error as e:
@@ -107,8 +85,12 @@ def authenticate_google_drive():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+<<<<<<< HEAD
+            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_path, SCOPES)
+=======
             flow = InstalledAppFlow.from_client_secrets_file(
                 client_secrets_path, SCOPES)
+>>>>>>> 1f33cc5dded2753576eac642f6dd13434c6082a0
             creds = flow.run_local_server(port=0)
         with open(token_path, 'w') as token:
             token.write(creds.to_json())
@@ -116,6 +98,27 @@ def authenticate_google_drive():
     return build('drive', 'v3', credentials=creds)
 
 # Função para fazer o upload do arquivo para o Google Drive
+<<<<<<< HEAD
+def upload_file_to_drive(service, file, folder_id=None):
+    file_metadata = {'name': file.name}
+    if folder_id:
+        file_metadata['parents'] = [folder_id]
+    media = MediaFileUpload(file, resumable=True)
+    file_drive = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    return file_drive.get('id')
+
+# Função para limpar o formulário
+def clear_form():
+    fields = [
+        'company_name', 'website', 'website_no_site', 'client_type', 'contact_name',
+        'city', 'email', 'phone_dd', 'phone_number', 'market_segment', 'address',
+        'no_physical_address', 'capital', 'desired_revenue', 'services', 'payment_methods',
+        'source', 'business_field', 'business_type', 'context', 'return_time',
+        'market_analysis', 'difficulties', 'cnpj_or_cpf', 'employees'
+    ]
+    for field in fields:
+        st.session_state[field] = ''
+=======
 def upload_file_to_drive(service, file_path, folder_id=None):
     file_metadata = {'name': os.path.basename(file_path)}
     if folder_id:
@@ -151,6 +154,7 @@ def clear_form():
     st.session_state['difficulties'] = ''
     st.session_state['cnpj_or_cpf'] = ''
     st.session_state['employees'] = ''
+>>>>>>> 1f33cc5dded2753576eac642f6dd13434c6082a0
 
 # Streamlit app
 st.title("Levantamento de Perfil do Cliente")
@@ -161,21 +165,10 @@ create_table()
 # Adiciona CSS para melhorar o layout do formulário
 st.markdown("""
     <style>
-        .reportview-container {
-            background: url('assets/bg.png') no-repeat center center fixed; 
-            background-size: cover;
-        }
-        .sidebar .sidebar-content {
-            background: rgba(255, 255, 255, 0.8); /* White background with opacity for the sidebar */
-        }
-        .block-container {
-            max-width: 1200px; /* Define the maximum width of the form */
-            padding: 2rem; /* Add padding for better spacing */
-            margin: auto; /* Center the form horizontally */
-        }
-        .stTextInput, .stTextArea, .stCheckbox, .stSelectbox, .stMultiselect {
-            margin-bottom: 1rem; /* Add space between form fields */
-        }
+        .reportview-container { background: url('assets/bg.png') no-repeat center center fixed; background-size: cover; }
+        .sidebar .sidebar-content { background: rgba(255, 255, 255, 0.8); }
+        .block-container { max-width: 1200px; padding: 2rem; margin: auto; }
+        .stTextInput, .stTextArea, .stCheckbox, .stSelectbox, .stMultiselect { margin-bottom: 1rem; }
     </style>
 """, unsafe_allow_html=True)
 
@@ -185,15 +178,16 @@ submitted = False
 # Formulário de entrada
 with st.form(key='profile_form'):
     # Ajuste das colunas
-    col1, col2, col3 = st.columns([4, 4, 4])  # Ajustando a largura das colunas para mais espaço horizontal
+    col1, col2, col3 = st.columns([4, 4, 4])
 
     with col1:
         company_name = st.text_input("Nome da Empresa/Cliente")
         website = st.text_input("Site")
         website_no_site = st.checkbox("Não possuo um site")
-        client_type = st.multiselect("Tipo de Cliente (Escolha todas as opções que se aplicam)", 
-                                    ["Empresário", "MEI", "Gestor de Projetos", "Stakeholder", "Startup", "Holding", 
-                                     "Diretor", "Sócio", "CEO", "Gerente", "Assessor", "Investidor", "Outro"])
+        client_type = st.multiselect("Tipo de Cliente", 
+                                     ["Empresário", "MEI", "Gestor de Projetos", "Stakeholder", 
+                                      "Startup", "Holding", "Diretor", "Sócio", "CEO", 
+                                      "Gerente", "Assessor", "Investidor", "Outro"])
         contact_name = st.text_input("Nome do Contato")
         city = st.text_input("Cidade")
         email = st.text_input("E-mail")
@@ -202,10 +196,21 @@ with st.form(key='profile_form'):
         phone = f"({phone_dd}) {phone_number}"
 
     with col2:
-        market_segment = st.multiselect("Segmento de Mercado (Escolha todas as opções que se aplicam)", 
-                                        ["B2B", "B2C", "Ainda não decidi"])   
+        market_segment = st.multiselect("Segmento de Mercado", ["B2B", "B2C", "Ainda não decidi"])
         address = st.text_input("Endereço Físico")
         no_physical_address = st.checkbox("Não possuo endereço físico")
+<<<<<<< HEAD
+        capital = st.selectbox("Capital Disponível", ["20mil", "40mil", "60mil", "80mil", 
+                                                     "100mil", "200mil", "400mil", "600mil", 
+                                                     "800mil", "1 milhão", "Mais de 1 milhão"])
+        desired_revenue = st.text_input("Receita Desejada")
+        services = st.multiselect("Serviços Desejados", ["Consultoria", "Desenvolvimento Web", 
+                                                         "Marketing Digital", "Gestão de Projetos", "Outros"])
+        payment_methods = st.multiselect("Formas de Pagamento", ["Pix", "Boleto", "Cartão de Crédito", 
+                                                                 "Transferência", "PayPal", "Outros"])
+        source = st.text_input("Fonte de Aquisição")
+
+=======
         
         capital_options = ["20mil", "40mil", "60mil", "80mil", "100mil", "200mil", "400mil", "600mil", "800mil", "1 milhão", "Mais de 1 milhão"]
         capital = st.selectbox("Capital Disponível", capital_options)
@@ -220,6 +225,7 @@ with st.form(key='profile_form'):
         
         source = st.text_input("Fonte de Aquisição")
         
+>>>>>>> 1f33cc5dded2753576eac642f6dd13434c6082a0
     with col3:
         business_field = st.text_input("Área de Atuação")
         business_type = st.text_input("Tipo de Negócio")
@@ -231,8 +237,13 @@ with st.form(key='profile_form'):
         employees = st.text_input("Número de Funcionários")
 
     st.markdown("<hr>", unsafe_allow_html=True)
+<<<<<<< HEAD
+
+    # Upload de arquivos
+=======
     
     # Arquivos
+>>>>>>> 1f33cc5dded2753576eac642f6dd13434c6082a0
     st.subheader("Envio de Arquivos")
     uploaded_logo = st.file_uploader("Envie o Logo da Empresa", type=['jpg', 'jpeg', 'png'])
     uploaded_pdf = st.file_uploader("Envie o PDF", type=['pdf'])
@@ -243,6 +254,36 @@ with st.form(key='profile_form'):
     
     if submit_button:
         # Prepara os dados
+<<<<<<< HEAD
+        form_data = [
+            company_name, website, client_type, contact_name, email, phone, address, no_physical_address,
+            capital, desired_revenue, services, payment_methods, source, business_field, business_type,
+            context, return_time, market_analysis, difficulties, cnpj_or_cpf, employees
+        ]
+
+        # Salva os dados no banco de dados
+        insert_data(form_data, uploaded_logo.name if uploaded_logo else None, 
+                    uploaded_pdf.name if uploaded_pdf else None, 
+                    uploaded_video.name if uploaded_video else None)
+
+        # Autentica no Google Drive
+        drive_service = authenticate_google_drive()
+
+        # Faz upload dos arquivos, se fornecidos
+        if uploaded_logo:
+            upload_file_to_drive(drive_service, uploaded_logo)
+        if uploaded_pdf:
+            upload_file_to_drive(drive_service, uploaded_pdf)
+        if uploaded_video:
+            upload_file_to_drive(drive_service, uploaded_video)
+
+        st.success("Formulário enviado com sucesso!")
+        submitted = True
+
+# Limpar formulário após submissão bem-sucedida
+if submitted:
+    clear_form()
+=======
         form_data = [company_name, website, client_type, contact_name, email, phone, address, no_physical_address, 
                      capital, desired_revenue, services, payment_methods, source, business_field, business_type, 
                      context, return_time, market_analysis, difficulties, cnpj_or_cpf, employees]
@@ -274,3 +315,4 @@ with st.form(key='profile_form'):
         clear_form()
         submitted = True
         st.success("Formulário enviado com sucesso!")
+>>>>>>> 1f33cc5dded2753576eac642f6dd13434c6082a0
